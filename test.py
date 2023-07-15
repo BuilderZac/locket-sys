5b8bf5e393437d61409c672c93f9e1542c8692f1
BuilderZac <zacharyl2005@gmail.com>
2023-07-15 17:28:04 -0400

    requires release between presses

--- test.py
+++ test.py
@@ -5,13 +5,20 @@ up = Button(17)
 down = Button(27)
 select = Button(22)
 
+def rel(pin):
+    while pin.is_pressed:
+        sleep(0.05)
+
 while True:
     if up.is_pressed:
         print("up")
+        rel(up)
     if down.is_pressed:
         print("down")
+        rel(down)
     if select.is_pressed:
         print("select")
+        rel(select)
 
 
     sleep(0.05)
