c2aa8b67cadcc9de0be41b47cd0100ac16cf8f6f
BuilderZac <zacharyl2005@gmail.com>
2023-07-15 17:39:35 -0400

    made button module and added appList

---
+++ buttons.py
@@ -0,0 +1,21 @@
+from gpiozero import Button
+from time import sleep
+
+def rel(pin):
+    while pin.is_pressed:
+        sleep(0.05)
+
+up = Button(17)
+down = Button(27)
+select = Button(22)
+
+def getButton():
+    if up.is_pressed:
+        rel(up)
+        return "up"
+    if down.is_pressed:
+        rel(down)
+        return "down"
+    if select.is_pressed:
+        rel(select)
+        return "select"
--- test.py
+++ test.py
@@ -1,24 +1,4 @@
-from gpiozero import Button
-from time import sleep
+from buttons import getButton as get
 
-up = Button(17)
-down = Button(27)
-select = Button(22)
-
-def rel(pin):
-    while pin.is_pressed:
-        sleep(0.05)
-
-while True:
-    if up.is_pressed:
-        print("up")
-        rel(up)
-    if down.is_pressed:
-        print("down")
-        rel(down)
-    if select.is_pressed:
-        print("select")
-        rel(select)
-
-
-    sleep(0.05)
+if get == "up":
+    print("it works")
