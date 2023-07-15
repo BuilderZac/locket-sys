22e59e485b996e5a2af2387103079407a9f1b8e8
BuilderZac <zacharyl2005@gmail.com>
2023-07-15 17:52:13 -0400

    test

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ from buttons import getButton as get
 from time import sleep
 
 while True:
-    if get == 1:
+    if get() == 1:
         print("it works up")
-    if get == 2:
+    if get() == 2:
         print("it works down")
-    if get == 3:
+    if get() == 3:
         print("it works select")
     sleep(0.05)
