6ffe677c9ccb500081a5e42dc6b35636dfeed0ae
BuilderZac <zacharyl2005@gmail.com>
2023-07-24 14:28:12 -0400

    testing

--- startup.py
+++ startup.py
@@ -2,17 +2,17 @@ import time
 from PIL import Image,ImageDraw,ImageFont
 import c
 
-try:
-    epd = c.epd
-    epd.init()
-    epd.clear(255)
-    image = c.newImage()
-    draw = ImageDraw.Draw(image)
+#try:
+epd = c.epd
+epd.init()
+epd.clear(255)
+image = c.newImage()
+draw = ImageDraw.Draw(image)
 
-    draw.text((5,5), 'Happy Testing', font=c.font24, fill=0, align='center')
+draw.text((5,5), 'Happy Testing', font=c.font24, fill=0, align='center')
 
-    epd.init()
-    epd.Clear(0xFF)
-    epd.sleep()
-except:
-    print("Error")
+epd.init()
+epd.Clear(0xFF)
+epd.sleep()
+#except:
+#    print("Error")
