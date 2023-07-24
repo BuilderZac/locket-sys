fc639532d1fa03e6116fc2b29e29893bac26f95b
BuilderZac <zacharyl2005@gmail.com>
2023-07-24 15:06:53 -0400

    adding week days

--- startup.py
+++ startup.py
@@ -8,13 +8,14 @@ epd.init()
 epd.Clear(255)
 image = c.newImage()
 draw = ImageDraw.Draw(image)
-
 epd.displayPartBaseImage(epd.getbuffer(image))
+
+draw.text((35, 40), "Mon Tue Wen Thu Fri Sat Sun", font = c.font24, fill = 0)
 
 num = 0
 while (True):
-    draw.rectangle((75, 20, 175, 45), fill = 255)
-    draw.text((75, 20), time.strftime('%H:%M:%S'), font = c.font24, fill = 0)
+    draw.rectangle((75, 10, 175, 35), fill = 255)
+    draw.text((75, 10), time.strftime('%H:%M:%S'), font = c.font24, fill = 0)
     epd.displayPartial(epd.getbuffer(image))
     num = num + 1
     if(num == 60):
