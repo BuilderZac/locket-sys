4ac84b99b142091f3931373bbd2b592672c1497a
BuilderZac <zacharyl2005@gmail.com>
2023-07-24 15:30:28 -0400

    moving display parts

--- startup.py
+++ startup.py
@@ -11,13 +11,13 @@ draw = ImageDraw.Draw(image)
 epd.displayPartBaseImage(epd.getbuffer(image))
 
 x = datetime.date.today()
-draw.text((15, 40), "Mon Tue Wen Thu Fri Sat Sun", font = c.font18, fill = 0)
-draw.text((135, 5), x.strftime('%b/%d/%Y'), font = c.font24, fill = 0)
+draw.text((5, 40), "Mon Tue Wen Thu Fri Sat Sun", font = c.font18, fill = 0)
+draw.text((125, 5), x.strftime('%b/%d/%Y'), font = c.font24, fill = 0)
 
 num = 0
 while (True):
-    draw.rectangle((35, 5, 135, 30), fill = 255)
-    draw.text((35, 5), time.strftime('%H:%M:%S'), font = c.font24, fill = 0)
+    draw.rectangle((15, 5, 115, 30), fill = 255)
+    draw.text((15, 5), time.strftime('%H:%M:%S'), font = c.font24, fill = 0)
     epd.displayPartial(epd.getbuffer(image))
     num = num + 1
     if(num == 60):
