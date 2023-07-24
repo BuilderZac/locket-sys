a2f09cf1844ad33a3aeca737551d5cc6a19b7bac
BuilderZac <zacharyl2005@gmail.com>
2023-07-24 13:50:30 -0400

    test

--- startup.py
+++ startup.py
@@ -1,24 +1,27 @@
 import time
-from PIL import Image,ImageDraw
+from PIL import Image,ImageDraw,ImageFont
 from waveshare_epd import epd2in13_V3
+import os
 
-
-epd = epd2in13_V3.EPD()
-epd.init()
-epd.clear()
-time_image = Image.new('1', (epd.height, epd.width), 255)
-time_draw = ImageDraw.Draw(time_image)
+try:
+    epd = epd2in13_V3.EPD()
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    epd.init()
+    epd.clear()
+    time_image = Image.new('1', (epd.height, epd.width), 255)
+    time_draw = ImageDraw.Draw(time_image)
     
-epd.displayPartBaseImage(epd.getbuffer(time_image))
-num = 0
-while (True):
-    time_draw.rectangle((120, 80, 220, 105), fill = 255)
-    time_draw.text((120, 80), time.strftime('%H:%M:%S'), font = font24, fill = 0)
-    epd.displayPartial(epd.getbuffer(time_image))
-    num = num + 1
-    if(num == 30):
-        break
+    epd.displayPartBaseImage(epd.getbuffer(time_image))
+    num = 0
+    while (True):
+        time_draw.text((120, 80), time.strftime('%H:%M:%S'), font = font24, fill = 0)
+        epd.displayPartial(epd.getbuffer(time_image))
+        num = num + 1
+        if(num == 30):
+            break
     
-epd.init()
-epd.Clear(0xFF)
-epd.sleep()
+    epd.init()
+    epd.Clear(0xFF)
+    epd.sleep()
+except:
+    print("Error")
