03ff48db2a677beca82070662a7df13e7dfc4514
BuilderZac <zacharyl2005@gmail.com>
2023-07-24 15:15:00 -0400

    added month/day/year

--- startup.py
+++ startup.py
@@ -10,12 +10,13 @@ image = c.newImage()
 draw = ImageDraw.Draw(image)
 epd.displayPartBaseImage(epd.getbuffer(image))
 
-draw.text((25, 40), "Mon Tue Wen Thu Fri Sat Sun", font = c.font18, fill = 0)
+draw.text((15, 40), "Mon Tue Wen Thu Fri Sat Sun", font = c.font18, fill = 0)
+draw.text((15, 40), time.localtime(1)+"/"+time.localtime(2)+"/"+time.localtime(0), font = c.font18, fill = 0)
 
 num = 0
 while (True):
-    draw.rectangle((75, 10, 175, 35), fill = 255)
-    draw.text((75, 10), time.strftime('%H:%M:%S'), font = c.font24, fill = 0)
+    draw.rectangle((75, 5, 175, 30), fill = 255)
+    draw.text((75, 5), time.strftime('%H:%M:%S'), font = c.font24, fill = 0)
     epd.displayPartial(epd.getbuffer(image))
     num = num + 1
     if(num == 60):
