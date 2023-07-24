e05ded41b5b005b8c2d6700df0c3f9dde9f8edf2
BuilderZac <zacharyl2005@gmail.com>
2023-07-24 14:25:13 -0400

    trying to take a step back for more testing

--- c.py
+++ c.py
@@ -1,6 +1,12 @@
 #c is short for common
 from PIL import Image,ImageDraw,ImageFont
 import os
+from waveshare_epd import epd2in13_V3
 
 pic = 'pic'
 font24 = ImageFont.truetype(os.path.join('pic', 'Font.ttc'), 24)
+epd = epd2in13_V3.EPD()
+
+def newImage():
+    image  = Image.new(mode=1,size=(epd.height,epd.width),color=255)
+    return image
--- startup.py
+++ startup.py
@@ -1,25 +1,16 @@
 import time
 from PIL import Image,ImageDraw,ImageFont
-from waveshare_epd import epd2in13_V3
-import os
 import c
 
 try:
-    epd = epd2in13_V3.EPD()
+    epd = c.epd
     epd.init()
-    epd.clear()
-    time_image = Image.new('1', (epd.height, epd.width), 255)
-    time_draw = ImageDraw.Draw(time_image)
-    
-    epd.displayPartBaseImage(epd.getbuffer(time_image))
-    num = 0
-    while (True):
-        time_draw.text((120, 80), time.strftime('%H:%M:%S'), font = c.font24, fill = 0)
-        epd.displayPartial(epd.getbuffer(time_image))
-        num = num + 1
-        if(num == 30):
-            break
-    
+    epd.clear(255)
+    image = c.newImage()
+    draw = ImageDraw.Draw(image)
+
+    draw.text((5,5), 'Happy Testing', font=c.font24, fill=0, align='center')
+
     epd.init()
     epd.Clear(0xFF)
     epd.sleep()
