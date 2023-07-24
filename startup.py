75a50487e19a684247758cc807a6e4aa99d85842
BuilderZac <zacharyl2005@gmail.com>
2023-07-24 15:35:05 -0400

    test

--- startup.py
+++ startup.py
@@ -12,12 +12,12 @@ epd.displayPartBaseImage(epd.getbuffer(image))
 
 x = datetime.date.today()
 draw.text((5, 40), "Mon Tue Wen Thu Fri Sat Sun", font = c.font18, fill = 0)
-draw.text((125, 5), x.strftime('%b/%d/%Y'), font = c.font24, fill = 0)
+draw.text((117, 5), x.strftime('%b/%d/%Y'), font = c.font24, fill = 0)
 
 num = 0
 while (True):
-    draw.rectangle((15, 5, 115, 30), fill = 255)
-    draw.text((15, 5), time.strftime('%H:%M:%S'), font = c.font24, fill = 0)
+    draw.rectangle((12, 5, 112, 30), fill = 255)
+    draw.text((12, 5), time.strftime('%H:%M:%S'), font = c.font24, fill = 0)
     epd.displayPartial(epd.getbuffer(image))
     num = num + 1
     if(num == 60):
