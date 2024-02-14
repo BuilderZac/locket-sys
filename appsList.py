647b74cca313b8c8123994b4bdab1fd7d8a7f40a
BuilderZac <zacharyl2005@gmail.com>
2024-02-13 23:00:26 -0500

    started making the list work

--- appsList.py
+++ appsList.py
@@ -2,12 +2,29 @@ import c
 from PIL import Image, ImageDraw, ImageFont
 
 
-def appList():
-    print("Triggered appList()")
-
-
 epd = c.epd()
 epd.init()
 image = c.newImage()
 draw = ImageDraw.Draw(image)
 
+
+appList = {"", "Home", "Pull Github", "Shutdown", ""}
+
+
+def drawList(index):
+    draw.text((80, 5), appList[index - 1], font = c.font24, fill = 0)
+    draw.text((100, 5), appList[index], font = c.font24, fill = 0)
+    draw.text((120, 5), appList[index + 1], font = c.font24, fill = 0)
+
+def appList():
+    index = 1
+    drawList(index)
+    c.detectPress()
+    match c.getButton():
+        case 1:
+            index+1
+        case 2:
+            index-1
+        case 3:
+            print("selected")
+    print("Triggered appList()")
